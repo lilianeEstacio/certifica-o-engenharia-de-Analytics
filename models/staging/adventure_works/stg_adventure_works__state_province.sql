5f45c36fb9e9447cf0bca62b286be8fd004eac64
LILIANE ESTACIO <liliane.mkt.dig@gmail.com>
2025-02-19 18:17:28 +0000

    Documentação das tabelas staging

--- models/staging/adventure_works/stg_adventure_works__state_province.sql
+++ models/staging/adventure_works/stg_adventure_works__state_province.sql
@@ -8,7 +8,6 @@ renamed as (
 
     select
         cast(stateprovinceid as int) as state_province_id,
-        cast(territoryid as int) as territory_id,
         cast(stateprovincecode as string) as state_province_code,
         cast(countryregioncode as string) as country_region_code,
         cast(name as string) as state_name
@@ -20,7 +19,6 @@ final as (
 
     select
         {{ dbt_utils.generate_surrogate_key(['state_province_id']) }} as state_province_uid,
-        {{ dbt_utils.generate_surrogate_key(['territory_id']) }} as territory_uid,
         {{ dbt_utils.generate_surrogate_key(['country_region_code']) }} as country_region_uid,
         state_province_id,
         state_province_code,
